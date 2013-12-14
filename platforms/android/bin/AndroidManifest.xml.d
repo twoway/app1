2cd37a2db36723079a1df53e3845b95de0b124d0
Torbjörn Sjögren <torbjorn@twoway.se>
2013-12-14 23:15:34 +0100

    1st commit

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/scottopolis/Documents/Websites/www.wp4.dev/wp-content/apppresser-pg/platforms/android/bin/AndroidManifest.xml : \
+/Users/scottopolis/Documents/Websites/www.wp4.dev/wp-content/apppresser-pg/platforms/android/AndroidManifest.xml \
---
+++ platforms/android/bin/classes/R.java.d
@@ -0,0 +1,9 @@
+/Users/scottopolis/Documents/Websites/www.wp4.dev/wp-content/apppresser-pg/platforms/android/gen/com/apppresser/app1/R.java \
+ : /Users/scottopolis/Documents/Websites/www.wp4.dev/wp-content/apppresser-pg/platforms/android/res/drawable/icon.png \
+/Users/scottopolis/Documents/Websites/www.wp4.dev/wp-content/apppresser-pg/platforms/android/res/drawable-hdpi/icon.png \
+/Users/scottopolis/Documents/Websites/www.wp4.dev/wp-content/apppresser-pg/platforms/android/res/drawable-ldpi/icon.png \
+/Users/scottopolis/Documents/Websites/www.wp4.dev/wp-content/apppresser-pg/platforms/android/res/drawable-mdpi/icon.png \
+/Users/scottopolis/Documents/Websites/www.wp4.dev/wp-content/apppresser-pg/platforms/android/res/drawable-xhdpi/icon.png \
+/Users/scottopolis/Documents/Websites/www.wp4.dev/wp-content/apppresser-pg/platforms/android/res/values/strings.xml \
+/Users/scottopolis/Documents/Websites/www.wp4.dev/wp-content/apppresser-pg/platforms/android/res/xml/config.xml \
+/Users/scottopolis/Documents/Websites/www.wp4.dev/wp-content/apppresser-pg/platforms/android/bin/AndroidManifest.xml \
